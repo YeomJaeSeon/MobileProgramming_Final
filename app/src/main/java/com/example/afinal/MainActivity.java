ae1dd5a5f8c16b3e99d384cd749142cb6ac2aa57
YeomJaeSeon <a89541457@gmail.com>
2020-11-02 19:25:04 +0900

    유성받아라

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
