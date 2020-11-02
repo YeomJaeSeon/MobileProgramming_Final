8861934ff228032a2c0ba287c756847a78136c97
YeomJaeSeon <a89541457@gmail.com>
2020-11-02 20:01:46 +0900

    재

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void click(View v){
+        int btnId = v.getId();
+
+        switch(btnId){
+            case R.id.status:
+                break;
+            case R.id.home:
+                break;
+            case R.id.planner:
+                break;
+        }
     }
 }
