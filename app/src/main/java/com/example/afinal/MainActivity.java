f05ea20f59a19361b7579515f0b4de93a73ea3e3
YeomJaeSeon <a89541457@gmail.com>
2020-11-02 20:24:32 +0900

    nono

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
+import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
